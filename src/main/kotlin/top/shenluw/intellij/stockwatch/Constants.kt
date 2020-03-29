92572833d30c01550dad60932a8351b680176d76
luws <luw1683@163.com>
2020-03-29 20:52:54 +0800

    up

--- src/main/kotlin/top/shenluw/intellij/stockwatch/Constants.kt
+++ src/main/kotlin/top/shenluw/intellij/stockwatch/Constants.kt
@@ -10,8 +10,8 @@ const val WSS_API_ADDRESS = "wss://openapi.itiger.com:8887/stomp"
 const val HTTP_API_ADDRESS = "https://openapi.itiger.com/gateway"
 //val API_ADDRESS = "wss://openapi-sandbox.itiger.com:8889/stomp"
 
-const val PLUGIN_ID = "StockWatch"
-const val PLUGIN_NAME = "stock watch"
+const val PLUGIN_ID = "top.shenluw.intellij.stock-watch"
+const val PLUGIN_NAME = "Stock Watch"
 const val TIGER_HELP_LINK = "https://quant.itiger.com/openapi/java-docs/zh-cn/docs/intro/quickstart.html"
 
 val QuotesTopic = Topic.create("Quotes", QuotesService.QuotesListener::class.java)
